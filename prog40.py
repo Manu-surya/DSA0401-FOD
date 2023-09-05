2c384ae4959eb5ea1652701f185a56fd48e40333
Manu-surya <112241247+Manu-surya@users.noreply.github.com>
2023-09-05 09:17:46 +0530

    prog40.py

--- prog40.py
+++ prog40.py
@@ -1,19 +1,19 @@
 import pandas as pd
-
-# Read the CSV file into a DataFrame
-df = pd.read_csv('soccer_players.csv')
-# Top 5 players by goals scored
-top_players_goals = df.nlargest(5, 'Goals_Scored')
-
-# Top 5 players by salaries
-top_players_salaries = df.nlargest(5, 'Weekly_Salary')
-average_age = df['Age'].mean()
-above_average_age_players = df[df['Age'] > average_age]['Name']
 import matplotlib.pyplot as plt
+df = pd.read_csv('soccer_players.csv')
+top_goals_players = df.nlargest(5, 'Goals_Scored')
+top_salary_players = df.nlargest(5, 'Weekly_Salary')
+average_age = df['Age'].mean()
+above_average_age_players = df[df['Age'] > average_age]
+print("Top 5 Players with the Highest Goals Scored:")
+print(top_goals_players)
+print("\nTop 5 Players with the Highest Salaries:")
+print(top_salary_players)
+print(f"\nAverage Age of Players: {average_age:.2f}")
+print("\nPlayers Above Average Age:")
+print(above_average_age_players[['Name', 'Age']])
 
 position_counts = df['Position'].value_counts()
-position_counts.plot(kind='bar')
-plt.xlabel('Position')
-plt.ylabel('Count')
-plt.title('Distribution of Players by Position')
+position_counts.plot(kind='bar', xlabel='Position', ylabel='Count', title='Distribution of Players by Position')
+plt.xticks(rotation=45)
 plt.show()
